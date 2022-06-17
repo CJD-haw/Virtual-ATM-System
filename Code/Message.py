2b8a5d65d9c60484772c9fd4dd6129e81f452f6b
Jeya Suriya <93221485+CJD-haw@users.noreply.github.com>
2022-06-17 23:02:59 +0530

    Update Message.py

--- Code/Message.py
+++ Code/Message.py
@@ -5,19 +5,9 @@ from geopy.geocoders import Nominatim
 location = Nominatim(user_agent="atm")
 address = location.geocode("Alangulam")
 
-SID = [
-    'AC3ec941c780601b0746e7142d91322dc9',
-    'AC208b1adfa448702027fe3d965dc92d86',
-    'AC7f6bcb433d0c80c8602676daa91b1988',
-    'AC100204cedf61f90653375cbdfda7a4ec',
-    ]
+SID = []
 
-TOKEN = [
-    'ada97108ed6c5ab2677e539905b3de68',
-    '5da58f77356d20180121d64bc78e2c96',
-    '5ac8a3a43463b2b59a33e36cfe11ef29',
-    'c719c95d7ca1222b6347ca1ee55917c3',
-    ]
+TOKEN = []
 
 SENDER = [
     '+14432412447',
